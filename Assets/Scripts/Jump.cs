7bfe13df73f92a1e591dbc3b812b343d129e1ab9
Runkai Zhang <1105420698@qq.com>
2020-01-13 22:44:10 -0500

    Added basic animation and movement mechanics

--- Assets/Scripts/Jump.cs
+++ Assets/Scripts/Jump.cs
@@ -3,16 +3,19 @@
 public class Jump : MonoBehaviour
 {
     public float jumpSpeed = 5f;
+    Animator animator;
     public bool isGrounded;
     Rigidbody rb;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
     }
     void OnCollisionEnter(Collision col)
     {
         if (col.gameObject.tag == ("Ground") && isGrounded == false)
         {
+            animator.SetBool("isJumping", false);
             isGrounded = true;
         }
     }
@@ -20,6 +23,7 @@ public class Jump : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
+            animator.SetBool("isJumping", true);
             rb.AddForce(new Vector3(0, 2, 0) * jumpSpeed, ForceMode.Impulse);
             isGrounded = false;
         }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,53 +4,75 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Camera targetCamera;
+    Animator animator;
     private Rigidbody rg;
     public float speed = 10.0f;
-    public float speedH = 2.0f;
-    public float speedV = 2.0f;
-
-    private float yaw = 0.0f;
-    private float pitch = 0.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         rg = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
         rg.freezeRotation = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        yaw += speedH * Input.GetAxis("Mouse X");
-        pitch -= speedV * Input.GetAxis("Mouse Y");
+        transform.rotation = Quaternion.Euler(transform.eulerAngles.x, targetCamera.transform.eulerAngles.y, transform.eulerAngles.z);
+        if (!Input.anyKey)
+        {
+            animator.SetBool("isWalking", false);
+        }
+    }
 
-        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+    void FixedUpdate()
+    {
         move();
     }
 
     void move()
     {
+        float speedMultiplier = 1f;
         Vector3 totalMovement = Vector3.zero;
 
         if (Input.GetKey(KeyCode.W))
         {
+            animator.SetBool("isWalking", true);
             totalMovement += transform.forward;
         }
         if (Input.GetKey(KeyCode.A))
         {
+            animator.SetBool("isWalking", true);
             totalMovement -= transform.right;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            totalMovement += transform.right;
         }
         if (Input.GetKey(KeyCode.S))
         {
+            animator.SetBool("isWalking", true);
             totalMovement -= transform.forward;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            animator.SetBool("isWalking", true);
+            totalMovement += transform.right;
+        }
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            animator.SetBool("isRunning", true);
+            speedMultiplier = 1.5f;
+        }
+        else
+        {
+            animator.SetBool("isRunning", false);
+        }
+
+        if (!Input.anyKey)
+        {
+            animator.SetBool("isWalking", false);
         }
 
         // To ensure same speed on the diagonal, we ensure its magnitude here instead of earlier
-        rg.MovePosition(transform.position + totalMovement.normalized * speed * Time.deltaTime);
+        rg.MovePosition(transform.position + totalMovement.normalized * speed * speedMultiplier * Time.deltaTime);
     }
 }
